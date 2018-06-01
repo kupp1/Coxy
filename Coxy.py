0f0b00d77dae136da13bfa70ac0f40699c86072a
kupp <kupp@debian>
2018-06-01 07:42:53 +0300

    add Coxy wirking version

--- Coxy.py
+++ Coxy.py
@@ -36,8 +36,8 @@ def pass_check(n):
 password = pass_check(3)
 
 host = 'irc.run.net'
-port = 6660
-nick = 'Coxy_t'
+port = 6660 #use utf-8
+nick = 'Coxy'
 username = 'Coxy'
 realname = 'kupp bot'
 
@@ -46,7 +46,7 @@ bot_hoster = 'kupp' #nick
 prefix = '.'
 channels = [
     '#16bits',
-    # '#16bit'
+    '#16bit'
 ]
 
 base = open('base.txt')
@@ -111,6 +111,19 @@ def start_dance(sock, data):
     time.sleep(3)
     kirc.send_privmsg(sock, kirc.sender_ch_find(data), dance.get_dance_3(kirc.get_names(sock, kirc.sender_ch_find(data)), nick))
 
+def send_dance_top(sock, recipient):
+    top = dance.get_top_dacers()
+    kirc.send_privmsg(sock, recipient, dance.get_top_start())
+    for i in range(len(top)):
+        kirc.send_privmsg(sock, recipient, '\x0302' + top[i].split()[0] + '\x03 : \x0304 ' + top[i].split()[1] + '\x03')
+    kirc.send_privmsg(sock, recipient, dance.get_top_end())
+
+def privmsg_priv_check(msg):
+    if msg.find('PRIVMSG ' + nick) != -1:
+        return True
+    else:
+        return False
+
 connected = False
 def loop(sock):
     try:
@@ -135,70 +148,62 @@ def loop(sock):
                 kirc.rejoin(sock, kirc.sender_ch_find(data))
             msg = kirc.get_real_privmsg(data)
             command = ''
-            print(msg)
             if msg:
                 command = get_command(msg)
-            if command and (data.find('PRIVMSG ' + nick) == -1):
+                priv = privmsg_priv_check(data)
+            if command:
+                if privmsg_priv_check(data) == False:
+                    recipient = kirc.sender_ch_find(data)
+                else:
+                    recipient = kirc.sender_nick_find(data)
                 if command == 'uptime':
-                    time_diff = datetime.datetime.now() - start_time
-                    seconds = int(time_diff.total_seconds())
-                    minutes = 0
-                    hours = 0
-                    days = 0
-                    if seconds > 60:
-                        minutes = int(seconds // 60)
-                        seconds = int(seconds % 60)
-                    if minutes > 60:
-                        hours = int(minutes // 60)
-                        minutes = int(hours % 60)
-                    if hours > 24:
-                        days = int(hours// 24)
-                        hours = int(hours % 24)
-                    kirc.send_privmsg(sock, kirc.sender_ch_find(data), get_bot_uptime())
+                    kirc.send_privmsg(sock, recipient, get_bot_uptime())
                 if command == 'host_uptime':
-                    kirc.send_privmsg(sock, kirc.sender_ch_find(data), uptime.get_host_machine_uptime())
+                    kirc.send_privmsg(sock, recipient, uptime.get_host_machine_uptime())
                 if command == 'help':
                     kirc.send_notice(sock, kirc.sender_nick_find(data), help_str)
                 if (command == 'boobs') or (command == 'сиськи'):
-                    if delay.delay(senders_boobs_nick_list, senders_boobs_nick_time, send_timer, kirc.sender_nick_find(data) + '_at_' + kirc.sender_ch_find(data)) == True:
-                        kirc.send_privmsg(sock, kirc.sender_ch_find(data), parsers.boobs())
+                    if delay.delay(senders_boobs_nick_list, senders_boobs_nick_time, send_timer, kirc.sender_nick_find(data)) == True:
+                        kirc.send_privmsg(sock, recipient, parsers.boobs())
                     else:
                         kirc.send_notice(sock, kirc.sender_nick_find(data), 'delay 120 seconds')
                 if (command == 'butts') or (command == 'жопки'):
-                    if delay.delay(senders_butts_nick_list, senders_butts_nick_time, send_timer, kirc.sender_nick_find(data) + '_at_' + kirc.sender_ch_find(data)) == True:
-                        sock.send((str('PRIVMSG ' + kirc.sender_ch_find(data) + ' :' + parsers.butts() + ' \r\n').encode()))
+                    if delay.delay(senders_butts_nick_list, senders_butts_nick_time, send_timer, kirc.sender_nick_find(data)) == True:
+                        sock.send((str('PRIVMSG ' + recipient + ' :' + parsers.butts() + ' \r\n').encode()))
                     else:
                         kirc.send_notice(sock, kirc.sender_nick_find(data), 'delay 120 seconds')
                 if command == nick:
-                    if delay.delay(senders_nick_list, senders_nick_time, send_timer, kirc.sender_nick_find(data) + '_at_' + kirc.sender_ch_find(data)) == True:
+                    if delay.delay(senders_nick_list, senders_nick_time, send_timer, kirc.sender_nick_find(data)) == True:
                         index = random.randint(0, len(Coxy))
                         if arg:
                             try:
-                                kirc.send_privmsg(sock, kirc.sender_ch_find(data), Coxy[int(arg)])
+                                kirc.send_privmsg(sock, recipient, Coxy[int(arg)])
                             except:
                                 delay.last_force_del(senders_nick_list, senders_nick_time)
                                 kirc.send_notice(sock, kirc.sender_nick_find(data), 'Enter only citation number after command motherfucker!')
 
                         else:
-                            while delay.delay(last_citations_list, last_citations_time, citations_timer, str(index) + '_at_' + kirc.sender_ch_find(data)) != True:
+                            while delay.delay(last_citations_list, last_citations_time, citations_timer, str(index)) != True:
                                 index = random.randint(0, len(Coxy))
-                            kirc.send_privmsg(sock, kirc.sender_ch_find(data), Coxy[index])
+                            kirc.send_privmsg(sock, recipient, Coxy[index])
                     else:
                         kirc.send_notice(sock, kirc.sender_nick_find(data), 'delay 120 seconds')
-                if command == 'dance':
+                if (command == 'dance') and (priv == False):
                     if arg:
                         if arg == 'top':
                             kirc.send_notice(sock, kirc.sender_nick_find(data), 'I send statistics as private message')
-                            top = dance.get_top_dacers()
-                            kirc.send_privmsg(sock, kirc.sender_nick_find(data), dance.get_top_start())
-                            for i in range(len(top)):
-                                kirc.send_privmsg(sock, kirc.sender_nick_find(data), '\x0302' + top[i].split()[0] + '\x03 : \x0304 ' + top[i].split()[1] + '\x03')
-                            kirc.send_privmsg(sock, kirc.sender_nick_find(data), dance.get_top_end())
+                            send_dance_top(sock, recipient)
                     else:
                         if delay.delay(dance_list, dance_time, dance_timer, kirc.sender_ch_find(data)) == True:
-                            start_dance(sock, data)
+                            start_dance(sock, kirc.sender_nick_find(data))
                         else:
                             kirc.send_privmsg(sock, kirc.sender_ch_find(data), 'Маэстро приходит один раз в день!')
+                elif (command == 'dance') and (priv == True):
+                    if arg == 'top':
+                        kirc.send_notice(sock, kirc.sender_nick_find(data), 'I send statistics as private message')
+                        send_dance_top(sock, recipient)
+                    else:
+                        kirc.send_privmsg(sock, kirc.sender_nick_find(data), 'Эта команда только для каналов')
 
     except:
         sock = kirc.reload_sock(sock)
